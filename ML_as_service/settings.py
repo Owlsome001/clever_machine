40de69804eb1f44e0f04c0c81a1ace78a68a46f7
Egide Mushoko <103942607+Owlsome001@users.noreply.github.com>
2023-04-06 22:58:42 +0000

    modified:   ML_as_service/__pycache__/settings.cpython-310.pyc 	modified:   ML_as_service/settings.py 	modified:   db.sqlite3

--- ML_as_service/settings.py
+++ ML_as_service/settings.py
@@ -128,7 +128,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-CSRF_TRUSTED_ORIGINS = ['https://*.preview.app.github.dev','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://*.github.dev','https://*.127.0.0.1']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
